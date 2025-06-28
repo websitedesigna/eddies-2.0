d1cb93c2390f8f9fa7a781747955b3d591bf3fd5
Riley Oliver <roswebsitedesign@gmail.com>
2025-06-28 11:55:41 +0100

    Update File

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/eddies-2.0/',
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
 });
